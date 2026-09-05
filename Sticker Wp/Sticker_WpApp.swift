be89a0156fef6aacdc0ffbab97d2e0a5bbb6a47f
matrexdev <matrexwutsu@gmail.com>
2026-09-05 14:10:44 +0300

    Initial Commit

---
+++ Sticker Wp/Sticker_WpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sticker_WpApp.swift
+//  Sticker Wp
+//
+//  Created by Aziz Yigit on 5.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct Sticker_WpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
